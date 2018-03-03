827b4ee21a0799bf63c730745fa8bb9048df143c
Andreas Dewes <andreas.dewes@7scientists.com>
2018-03-03 13:39:53 +0100

    Bumped version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='beam',
     python_requires='>=3',
-    version='0.13',
+    version='0.14',
     author='Andreas Dewes - 7scientists',
     author_email='andreas.dewes@7scientists.com',
     license='MIT',
