80f0cd7d4488c51229366098f6680cc722837d79
Andreas Dewes <andreas.dewes@7scientists.com>
2018-04-25 18:12:24 +0200

    v0.36

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='beam-up',
     python_requires='>=3',
-    version='0.35',
+    version='0.36',
     author='Andreas Dewes - 7scientists',
     author_email='andreas.dewes@7scientists.com',
     license='MIT',
